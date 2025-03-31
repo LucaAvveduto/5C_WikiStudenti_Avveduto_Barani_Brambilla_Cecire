7a902fc3d63d19b40b0d2421896326157b7bf376
Simone Cecire <ceciresimone@itis-molinari.eu>
2025-03-31 18:24:31 +0200

    modifiche generali

---
+++ src/scripts/GUI/navigator/navigator.js
@@ -0,0 +1,25 @@
+export const generateNavigator = (parentElement) => {
+    const pages = Array.from(parentElement.querySelectorAll(".page"));
+
+    const hide = (element) => {
+        element.classList.add("is-hidden");
+    }
+    
+    const show = (element) => {
+        element.classList.remove("is-hidden");
+    }
+
+    const render = () => {
+        const url = new URL(document.location.href);
+        const pageName = url.hash.replace("#", "");
+        const selectedPage = pages.filter((page) => page.id === pageName)[0] || pages[0];
+        document.title = selectedPage.id.replace("article-", "").replaceAll("-", " ") + " | WikiStudenti";
+        document.title = document.title[0].toUpperCase() + document.title.substring(1);
+
+        pages.forEach(p => hide(p));
+        show(selectedPage);
+    }
+
+    window.addEventListener("popstate", render); 
+    render();   
+}
---
+++ src/scripts/GUI/searchbar/searchbar.js
@@ -0,0 +1,8 @@
+export const generateSearchbar = () => {
+    return {
+        build: () => {
+
+        },
+        
+    };
+};
