3bc9fd1d04362232658bad72bc006617998ee562
Simone Barani <baranisimone@itis-molinari.eu>
2025-04-07 12:04:22 +0200

    inizio middleware

--- src/scripts/middleware/middleware.js
+++ src/scripts/middleware/middleware.js
@@ -1,6 +1,6 @@
 export const generateMiddleware = () => {
     return {
-        login: async (username, password) => {
+        login: async (username, password) => {  //login utente
             try {
                 const res = await fetch("");
                 const data = await res.json();
@@ -9,8 +9,85 @@ export const generateMiddleware = () => {
                 console.error(error);
             }
         },
-        register: async (user) => {
+        register: async (user) => { //registrazione utente
             
+        },
+
+    load: ()=>{
+        try {
+            return new Promise((resolve, reject) => {
+                fetch("/bookings").then(r => r.json()).then(data => {
+                    resolve(data);
+                }).catch(err => {
+                    reject(err);
+                })
+            });
         }
-    };
-};
+        catch (e) {
+            console.error(e);
+        }
+    },
+
+    delete: (id) =>{ //rimozione utente
+        try {
+            return new Promise((resolve, reject) => {
+                fetch("/delete/"+id, {
+                    method: "delete",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }).then(r => r.json()).then(data => {
+                    return resolve(data);
+                }).catch(err => {
+                    reject(err);
+                })
+            });
+        }
+        catch (e) {
+            console.error(e);
+        }
+    },
+    insert: (article) => {  //aggiunta articolo
+        const body = article;
+        const fetchOptions = {
+            method: 'post',
+            body: body
+        };
+
+        try {
+            return new Promise((resolve, reject) => {
+                fetch("/insert", fetchOptions).then(r => r.json()).then(data => {
+                    resolve(data);
+                }).catch(err => {
+                    reject(err);
+                })
+            });   
+        }
+        catch (e) {
+            console.error(e);
+        }
+    },
+
+    modificaDati: async (user) => {
+            
+    },
+
+    modificaRuoli: async (user) => {
+            
+    },
+
+    approvazione: async (article) => {
+            
+    },
+
+    ripristino: async (version) => {
+            
+    },
+
+    richiestaDati: async () => {
+            
+    }
+
+    }
+}
+
